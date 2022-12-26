9b3bd36cf187dfb13f902620a4a35f5d43e08ef8
tao <17695958740@163.com>
2022-12-26 18:40:02 +0800

    test github actions

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
+    test github actions...
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
